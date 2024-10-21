3a41757d307bf16c458867faec0d56c1de0632fd
Himanshu Kandpal <himanshukp324@gmail.com>
2024-10-21 07:29:09 +0530

    add task component and load the task as per the selected user

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { dummyTasks } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: false }) name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  tasks = dummyTasks;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
